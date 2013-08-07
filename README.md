7483dcc64d046c76c98b3e748443007399662e62
Stella Tigre <dearstellanebula@gmail.com>
2013-08-07 13:15:01 -0400

    Remove unneeded JS and begin work on querystring support Create README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+videodrone
+==========
+
+Videodrone is something of an interactive online art installation.  Given a keyword, it will produce a wall
+of 9 videos from youtube based on the search term.  It's intended both as an overload of digital stimuli,
+and a comment on pop culture in a way.
+
+It's implemented in JS, very simply, via the youtube api.
+
+hashtagyoloswag.org/videodrone
+
--- swag.js
+++ swag.js
@@ -1,8 +1,7 @@
 var api_key = 'AI39si5lXW19Z9VDP8scyfWe-n6myEmOCsOM_I-huxhGS_JVpB582jwulvgu0TotRCCFEZb1WuZBh9zzWd8l7EQ5c5gqkjxfxQ' ;
 var base_url = 'https://gdata.youtube.com/feeds/api/videos?alt=json&paid-content=false&max-results=50&q=';
-var iframe_begin = '<iframe width="632" height="360" src="//www.youtube.com/embed/';
-var i_mid = '?autoplay=1&vq=small&loop=1&playlist=';
-var i_end = '&rel=0" frameborder="0" allowfullscreen></iframe>';
+var iframe_begin = '<iframe width="640" height="360" src="//www.youtube.com/embed/';
+var iframe_end = '?autoplay=1&vq=small&loop=1&rel=0" frameborder="0" allowfullscreen></iframe>';
 var video_json = '';
 
 function random_int(min, max) {
@@ -21,29 +20,8 @@ function get_embeddable_id(entries) {
 	return get_id(r_entry);
 }
 
-function fill_video_divs() {
-		var entries = video_json['responseJSON']['feed']['entry']
-	if ($('#chorus').is(':checked')) {
-		var id = get_embeddable_id(entries);
-		for (d=1 ; d < 10 ; d++) {
-				$('#v'+d.toString()).html(iframe_begin+id+i_mid+id+i_end);
-		}  
-	} else {
-		var used_ids = [];
-		for (d=1 ; d < 10 ; d++) {
-			var id = get_embeddable_id(entries);
-		while (used_ids.indexOf(id) != -1) {
-			var id = get_embeddable_id(entries);
-		}
-		console.log(id); used_ids.push(id);
-		$('#v'+d.toString()).html(iframe_begin+id+i_mid+id+i_end);
-		}	
-	}
-}
-
 function load_video_json() {
 	var subject  = $('#subject').val();
-	console.log(subject); 
 	return $.ajax({
 	  url: base_url+subject+'&v=2',
 	  dataType: 'json',
@@ -55,7 +33,7 @@ function load_video_json() {
 			if ($('#chorus').is(':checked')) {
 			    var id = get_embeddable_id(entries);
 			    for (d=1 ; d < 10 ; d++) {
-		                $('#v'+d.toString()).html(iframe_begin+id+i_mid+id+i_end);
+		                $('#v'+d.toString()).html(iframe_begin+id+iframe_end);
 			    }  
 			} else {
 				var used_ids = [];
@@ -65,7 +43,7 @@ function load_video_json() {
 						var id = get_embeddable_id(entries);
 					}
 					console.log(id); used_ids.push(id);
-					$('#v'+d.toString()).html(iframe_begin+id+i_mid+id+i_end);
+					$('#v'+d.toString()).html(iframe_begin+id+iframe_end);
 					}	
 				}
 			}
@@ -83,8 +61,9 @@ $(document).ready(function() {
     var querystring=window.location.search.substring(1); 
     console.log(querystring);
     if (querystring != "") {
-   	parse_querystring(querystring);
-       $('search').val(querystring);
+   		parse_querystring(querystring);
+		console.log("querystring found");
+       	$('search').val(querystring);
     }
 
     $('#submit').click(function() {
